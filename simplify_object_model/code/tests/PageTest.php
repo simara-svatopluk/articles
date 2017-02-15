69f951fd5995ec8f022e62d2fd420cc5de14daaf
Svaťa Šimara <svatopluk.simara@gmail.com>
2017-02-16 00:40:14 +0100

    Do not use Ramsey\Uuid in tests

--- simplify_object_model/code/tests/PageTest.php
+++ simplify_object_model/code/tests/PageTest.php
@@ -3,18 +3,17 @@
 namespace ObjectModel;
 
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
 
 class PageTest extends TestCase {
 
 	public function testShow() {
-		$uuid = (string) Uuid::uuid4();
+		$uuid = '2a85a666-4bc3-4680-9ca2-616c377cce80';
 		$page = new Page($uuid, 'the whole content');
 		self::assertSame('the whole content', $page->show());
 	}
 
 	public function testChange() {
-		$uuid = (string) Uuid::uuid4();
+		$uuid = '2a85a666-4bc3-4680-9ca2-616c377cce80';
 		$page = new Page($uuid, 'the whole content');
 		$page->change('the new whole content');
 		self::assertSame('the new whole content', $page->show());
